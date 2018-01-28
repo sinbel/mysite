30fb60588ce279c15507f8e6c96843fef3f51ce6
sinbel <pookharev@yandex.ru>
2018-01-28 15:15:00 +0500

    Initialize repository

--- demo/views.py
+++ demo/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 
-#from django.http import HttpResponse
-
-#from .models import Question
-
 def demo(request):
     return render(request, 'demo/index.html')
 
 def home(request):
-	return render(request, 'demo/home.html')
+    return render(request, 'demo/home.html')
 
 def contacts(request):
-	return render(request, 'demo/contacts.html')
+    return render(request, 'demo/contacts.html')
